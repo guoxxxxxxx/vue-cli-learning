31521af4d10c6873098fbfc9b5f20ea826990fd1
guo_x <guo_x0315@163.com>
2022-03-26 18:15:05 +0800

    props配置

--- README.md
+++ README.md
@@ -10,6 +10,33 @@
 打标识:
 <h1 ref='xxx'>....</h1>
 获取:
-this.$re
+this.$refs.xxx
+```
+
+## 配置项
+
+**功能：** 让组件接受外部传过来的数据
+
+``` js
+(1) 传递数据
+	<Demo name='xxx'>
+(2) 接收数据
+    第一种方式(只接收):
+    	props:['name']
+	
+	第二种方式(限制类型):
+    	props:{
+            name:Number
+        }
+	第三种方式(限制类型、限制必要性、指定默认值)
+		props:{
+            name:{
+                type:String,
+                required:true,
+                defalut: 'value'
+            }
+        }
+	
+	备注: props时只读的，Vue底层会检测你对props的修改,如果进行了修改,就会发出警告, 若业务需求确实需要修改, 则需要赋值props的内容到data中,然后去修改data中的数据.
 ```
 
---
+++ src/components/StudentInfo.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="s">
+      <h2>姓名: {{name}}</h2>
+      <h2>年龄: {{age+1}}</h2>
+      <h2>性别: {{sex}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'StudentInfo',
+    data() {
+        return {
+        }
+    },
+    // 简单声明接收
+    // props:['name', 'age', 'sex']
+    
+    // 接收的同时对数据进行类型限制
+    // props:{
+    //     name: String,
+    //     age: Number,
+    //     sex: String,
+    // }
+
+    //  
+    props:{
+        name:{
+            type: String,
+            require:true,   // name是否必要
+        },
+        age:{
+            type: Number,
+            default: 30,        // 默认值
+        },
+        sex:{
+            type: String,
+            require: true
+        }
+    }
+}
+</script>
+
+<style>
+.s{
+    background-color: aliceblue;
+}
+</style>
