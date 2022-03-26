ec817f9e273a2e9761f1521f61ac24d34e9d0d7b
guo_x <guo_x0315@163.com>
2022-03-26 18:45:29 +0800

    mixin混合

---
+++ 04_src copy_mixin混合/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="root">
+    <SchoolName></SchoolName>
+    <StudentInfo name="李四" sex="男" :age='20'></StudentInfo>
+  </div>
+</template>
+
+<script>
+// 引入school组件
+import SchoolName from "./components/SchoolName.vue";
+import StudentInfo from "./components/StudentInfo.vue"
+export default {
+  components: { 
+      SchoolName,
+      StudentInfo,
+   },
+};
+</script>
+
+<style>
+</style>
