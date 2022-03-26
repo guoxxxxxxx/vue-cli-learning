acaf6c8ea02a75c42c86f45ef5d77ca224499945
guo_x <guo_x0315@163.com>
2022-03-26 21:32:22 +0800

    项目案例TodoList

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,87 @@
 <template>
   <div id="root">
-    <SchoolName></SchoolName>
-    <StudentInfo name="李四" sex="男" :age='20'></StudentInfo>
+    <div class="todo-container">
+      <div class="todo-wrap">
+        <header-component :receive="receive" />
+        <list-component :todos="todos"/>
+        <footer-component />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// 引入school组件
-import SchoolName from "./components/SchoolName.vue";
-import StudentInfo from "./components/StudentInfo.vue"
+import HeaderComponent from "./components/HeaderComponent.vue";
+import ListComponent from "./components/ListComponent.vue";
+import FooterComponent from "./components/FooterComponent.vue";
 export default {
-  components: { 
-      SchoolName,
-      StudentInfo,
-   },
+  name: "App",
+  components: {
+    HeaderComponent,
+    ListComponent,
+    FooterComponent,
+  },
+  data() {
+    return {
+      todos:[
+        {id: '001', title: '吃饭', completed: false},
+        {id: '002', title: '睡觉', completed: true},
+        {id: '003', title: '学习', completed: false},
+      ]
+    }
+  },
+  methods:{
+    receive(x){
+      this.todos.unshift(x);
+    }
+  }
 };
 </script>
 
 <style>
+/*base*/
+body {
+  background: #fff;
+}
+
+.btn {
+  display: inline-block;
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2),
+    0 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+}
+
+.btn-danger {
+  color: #fff;
+  background-color: #da4f49;
+  border: 1px solid #bd362f;
+}
+
+.btn-danger:hover {
+  color: #fff;
+  background-color: #bd362f;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+
+
 </style>
---
+++ src/components/ListComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul class="todo-main">
+    <item-component v-for="(item) in todos" :key="item.id" :item="item"/>
+  </ul>
+</template>
+
+<script>
+import ItemComponent from "./ItemComponent.vue";
+export default {
+  name: "ListComponent",
+  components: {
+    ItemComponent
+  },
+  props:{
+    todos:{
+      
+    }
+  }
+};
+</script>
+
+<style scoped>
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
