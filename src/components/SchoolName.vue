3a7bd0dd44d4f19b28e97ec2791a1068659394e0
guo_x <guo_x0315@163.com>
2022-03-26 17:16:37 +0800

    ref属性

---
+++ src/components/SchoolName.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="demo">
+      <h2>名称: {{name}}</h2>
+      <h2>地址: {{address}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'school-name',
+    data() {
+        return {
+            name: 'hebust',
+            address: 'sjz'
+        }
+    },
+}
+</script>
+
+<style>
+    .demo{
+        background-color: aqua;
+    }
+</style>
